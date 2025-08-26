ba7e6fded9d1867ba41b124ee5410f0adce9d9e3
Suyash1505 <adv.mks7171@gmail.com>
2025-08-26 11:27:27 +0530

    setup an environment

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+
+  return (
+    <main className='max-w-7xl'>
+      <h1 className='text-2xl text-white underline'>Three JS</h1>
+    </main>  
+  )
+}
+
+export default App
