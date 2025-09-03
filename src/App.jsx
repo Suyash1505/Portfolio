90715d705018975967d8bf9253b2c2ee4c3fb601
Suyash1505 <adv.mks7171@gmail.com>
2025-09-03 15:41:27 +0530

    add contact section

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,10 @@ import Navbar from './sections/Navbar'
 import Hero from './sections/Hero'
 import About from './sections/About'
 import Project from './sections/Project'
+import Contact from './sections/Contact'
 
 const App = () => {
+
 
   return (
     <main className='max-w-7xl mx-auto relative'>
@@ -12,6 +14,8 @@ const App = () => {
       <Hero />
       <About />
       <Project />
+
+      <Contact />
     </main>  
   )
 }
